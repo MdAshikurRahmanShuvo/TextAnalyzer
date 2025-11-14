706fedb706fe45fb95f86ecc0ca2bb053dbca10f
Md. Ashikur Rahman Shuvo <shuvo15-5633@diu.edu.bd>
2025-11-14 18:45:37 +0600

    use radio button and also add new features like remove new lines,remove spaces and count total length

--- textutils/views.py
+++ textutils/views.py
@@ -1,52 +1,49 @@
 #created by Ashik
-from django.http import HttpResponse
 from django.shortcuts import render
+
 def index(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 def analyzer(request):
-    djtext=request.GET.get('text','default')
-    removePunc=request.GET.get('removePunc',"off")
-    print(removePunc)
-    CapitalLetter=request.GET.get('CapitalLetter','off')
-    upperCase=request.GET.get('upperCase','off')
-    LowerCase=request.GET.get('lowerCase','off')
-    if(removePunc=='on'):
-        punctuations='''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
-        analyzed=""
+    djtext = request.GET.get('text', 'default')
+    action = request.GET.get('action', 'none')
+    new_line=""
+    # UPPERCASE
+    if action == "uppercase":
+        new_line = djtext.upper()
+
+    # lowercase
+    elif action == "lowercase":
+        new_line = djtext.lower()
+    elif(action=="removePunc"):
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        new_line = ""
+        for char in djtext:
+            if char not in punctuations:
+                new_line = new_line + char
+    # REMOVE spaces
+    elif action == "extraSpace":
+        
         for i in djtext:
-            if i not in punctuations:
-                analyzed=analyzed+i
-    elif(CapitalLetter=='on'):
-        analyzed=djtext.capitalize()
-    elif(upperCase=='on'):
-        analyzed=djtext.upper()
-    elif(LowerCase=='on'):
-        analyzed=djtext.lower()
-    elif(removePunc=='on' and CapitalLetter=='on'):
-        punctuations='''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
-        analyzed=""
-        for i in djtext:
-            if i not in punctuations:
-                analyzed=analyzed+i
-                analyzed=analyzed.capitalize()
-    elif(removePunc=='on' and upperCase=='on'):
-        punctuations='''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
-        analyzed=""
-        for i in djtext:
-            if i not in punctuations:
-                analyzed=analyzed+i
-                analyzed=analyzed.upper()
-    elif(removePunc=='on' and LowerCase=='on'):
-        punctuations='''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
-        analyzed=""
-        for i in djtext:
-            if i not in punctuations:
-                analyzed=analyzed+i
-                analyzed=analyzed.lower()
+            if i==" ":
+                new_line=new_line+""
+            else:
+                new_line=new_line+i
+    elif action=="newLine":
+        for j in djtext:
+            if j=="\n" or j=="\r":
+                new_line=new_line+""
+            else:
+                new_line=new_line+j
     else:
-        analyzed=djtext
-    removed={
-        'previous_text':djtext,
-        'value':analyzed,
+        l=0
+        for i in djtext:
+            l=l+1
+        new_line="Total characters are: "+str(l)
+
+    context = {
+        'previous_text': djtext,
+        'value': new_line,
     }
-    return render(request,'analyze.html',removed)
+
+    return render(request, 'analyze.html', context)
